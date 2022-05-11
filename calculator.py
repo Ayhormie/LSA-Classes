0e7c60d898cc549b8fccb98ad6500f279753856e
Ogunkanmi Michael <michaelogunkanmi@gmail.com>
2022-05-12 00:12:34 +0100

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def divide(a,b):
+    return a/b
+def addition(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def exponent(a,b):
+    return a**b
+def modulus(a,b):
+    return a%b
+def floor_div(a,b):
+    return a//b
